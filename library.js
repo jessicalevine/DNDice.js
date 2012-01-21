77402153ad4d0ff749dc62f487ac388a5b20cf83
Jordan Goldstein <mrjordangoldstein@gmail.com>
2012-01-21 21:45:35 +0000

    Finish major refactoring of javascript scope and functions in main.js

--- library.js
+++ library.js
@@ -1,10 +1,29 @@
-//Give all objects an inspect method for debugging
-Object.prototype.inspect = function() {
-    for(var property in this) {
-        console.log(property);
-    }
-};
-
-Object.prototype.alert = function() {
-    alert(this);
+var init_alfred = function() { 
+    var Alfred = {};
+    
+    //Give all objects an inspect method for debugging
+    Object.prototype.inspect = function() {
+        for(var property in this) {
+            console.log(property);
+        }
+    };
+    
+    //Allow objects to alert themselves
+    Object.prototype.alert = function() {
+        alert(this);
+    };
+    
+    //Give a set of methods for working with the DOM
+    Alfred.dom = (function () {
+        var dom = {};
+        
+        dom.from_id = function (id) {
+            return document.getElementById(id);
+        };
+        dom.val_from_id = function (id) {
+            return document.getElementById(id).value;
+        };
+        
+        return dom;
+    }());    
 };
--- main.js
+++ main.js
@@ -1,84 +1,96 @@
 var dndice = (function() {
-    var num = 0;
-    
-    //Actions taken on app initialization
+    var num_rolls = 0; //Tracks number of existing rolls
+   
     var main = function () {
         //Set triggers on DOM elements when DOM loads
         $(document).ready(function() {
             $("#statement").keyup(function (ev) {
                 if(ev.keyCode == 13){
-                    create_roll();
+                    handle();
                 }
             });   
             
             $("#submit").click(function () {
-                create_roll();
+                handle();
             });
         });
-    };
+    }; //Actions taken on app initialization
     
-    //Create a roll from form fields, and add it to the DOM
-    var create_roll = function () {
-        var roll = {
-            name: document.getElementById("name").value,
-            statement: document.getElementById("statement").value,
-            rolls: 0
-        };
+    var handle = function () {
+        var roll = init_roll(document.getElementById("name").value, document.getElementById("statement").value); //Create a roll object from DOM data
         
-        if (!roll.statement || !roll.statement) {
+        if (!roll.valid) {
             alert("Please complete the required fields");
             return;
-        }
+        } //Validate necessary data or stop creating roll
         
-        roll.dice = parse_dice(roll.statement);
+        num_rolls += 1; //Increment existing rolls
         
-        num += 1;    
-      
-        var text = num + ". " + roll.name + " (" + roll.statement + ") : ";
-        var text_node = document.createTextNode(text);
+        document.getElementById("dice").appendChild(roll.to_node()); //Add roll to DOM
         
-        var roll_node = document.createElement("input");
-        roll_node.setAttribute("type", "button");
-        roll_node.setAttribute("value", "Roll");
-        roll_node.setAttribute("id", "die" + num);
+        $("#die" + num_rolls).click(roll.handle); //Give the roll an event handler
+    }; //Create a roll from form fields and add it to DOM
+    
+    var init_roll  = function (name, statement) {
+        var roll = {};
         
-        var par = document.createElement("p");
-        par.appendChild(text_node);
-        par.appendChild(roll_node);
+        var parse_dice = function (statement) {
+            var values = statement.split("d");
+            return {
+                num: parseInt(values[0], 10),
+                size: parseInt(values[1], 10)
+            };
+        }; //Interpret a roll statement to a usable dice object 
+    
+        roll.name = name;
+        roll.statement = statement;
+        roll.rolls = 0;
         
-        document.getElementById("dice").appendChild(par);
+        roll.valid = function () {
+            return (roll.name || roll.statement);
+        }; //Validates a roll contains necessary data
         
-        $("#die" + num).click(function (ev) {
-            var res = " " + result(roll.dice);
+        roll.dice = (roll.valid) ? parse_dice(roll.statement) : null;
+        
+        roll.to_node = function () {
+            var text = num_rolls + ". " + roll.name + " (" + roll.statement + ") : ";
+            var text_node = document.createTextNode(text);
+            
+            var roll_node = document.createElement("input");
+            roll_node.setAttribute("type", "button");
+            roll_node.setAttribute("value", "Roll");
+            roll_node.setAttribute("id", "die" + num_rolls);
+            
+            var par = document.createElement("p");
+            par.appendChild(text_node);
+            par.appendChild(roll_node);
+            
+            return par;
+        }; //Return a DOM node representing the roll
+        
+        roll.result = function (die) {
+            var sum = 0;
+            for (var i = 0; i < die.num; i += 1) {
+                sum += Math.ceil(Math.random() * die.size);
+            }
+            return sum;
+        }; //Return the roll of a set of dice
+        
+        roll.handle = function (ev) {
+            var result = " " + roll.result(roll.dice);
             roll.rolls += 1;
             if(roll.rolls > 1) {
-                res = "," + res;
+                result = "," + result;
             }
-            ev.target.parentNode.appendChild(document.createTextNode(res));
-        });
-    }
-    
-    //Interpret a roll statement to a usable dice object 
-    var parse_dice = function (statement) {
-        var values = statement.split("d");
-        return {
-            num: parseInt(values[0], 10),
-            size: parseInt(values[1], 10)
-        };
-    }
-    
-    //Return the roll of a set of dice
-    var result = function (die) {
-        var sum = 0;
-        for (var i = 0; i < die.num; i += 1) {
-            sum += Math.ceil(Math.random() * die.size);
-        }
-        return sum;
-    }
+            ev.target.parentNode.appendChild(document.createTextNode(result));
+        }; //Event handler for activating a roll
+        
+        return roll;
+    }; //Initialize a roll object
     
     var app = {};
     app.main = main;
     return app;
-}());
+}()); //Wrap the entire app in a closure to avoid scope problems
 
 dndice.main();
